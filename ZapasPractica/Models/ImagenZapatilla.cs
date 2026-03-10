b24061419fbe53da959f21b656a2fa9d50c4343c
mmtol <mmigueltolosana@gmail.com>
2026-03-10 11:51:11 +0100

    Add project files.

---
+++ ZapasPractica/Models/ImagenZapatilla.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZapasPractica.Models
+{
+    [Table("IMAGENESZAPASPRACTICA")]
+    public class ImagenZapatilla
+    {
+        [Key]
+        [Column("IDIMAGEN")]
+        public int IdImagen { get; set; }
+        [Column("IDPRODUCTO")]
+        public int IdProducto { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+    }
+}
